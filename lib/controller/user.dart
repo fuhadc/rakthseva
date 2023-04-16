bd3931b6fed63cfba1b20de8d40b5901c274864e
fuhadc <fuhadcs@gmail.com>
2023-04-17 00:48:44 +0530

    req

--- lib/controller/user.dart
+++ lib/controller/user.dart
@@ -1,17 +1,17 @@
 class User {
   final String username;
   final String password;
-  final String? email;
-  final String? firstName;
-  final String? lastName;
+  final String email;
+  final String firstName;
+  final String lastName;
   final String userid; // add this field
 
   User(
       {required this.username,
       required this.password,
-      this.email,
-      this.firstName,
-      this.lastName,
+      required this.email,
+      required this.firstName,
+      required this.lastName,
       required this.userid});
 
   getUserDetails() {
