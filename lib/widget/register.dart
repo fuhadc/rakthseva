3af6676f93ac83ca1daa0ad1fc83d902deb103e7
fuhadc <fuhadcs@gmail.com>
2023-04-17 00:13:59 +0530

    jkh

--- lib/widget/register.dart
+++ lib/widget/register.dart
@@ -32,6 +32,9 @@ class _RegisterPageState extends State<RegisterPage> {
     try {
       final response = await http.post(
         Uri.parse('http://192.168.1.11:5555/register'),
+        headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
         body: json.encode(user.toJson()),
       );
 
