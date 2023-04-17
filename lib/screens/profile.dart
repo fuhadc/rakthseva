82688aa3dff2ee3df7e8b9b8a5e4a15bee3a069c
fuhadc <fuhadcs@gmail.com>
2023-04-17 06:38:32 +0530

    wd

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,63 +1,126 @@
 import 'package:flutter/material.dart';
+import 'certificate.dart';
 
-import '../controller/user.dart';
+class ProfilePage extends StatefulWidget {
+  final dynamic user;
 
-class ProfilePage extends StatelessWidget {
-  final User user;
+  const ProfilePage({super.key, required this.user});
 
-  ProfilePage({required this.user});
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _isDarkModeEnabled = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Profile'),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Username: ${user.username}',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              'Email: ${user.email}',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              'Name: ${user.firstName} ${user.lastName}',
-              style: TextStyle(fontSize: 20.0),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // handle request donation button press
-              },
-              child: Text('Request Donation'),
-            ),
-            SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: () {
-                // handle donation history button press
-              },
-              child: Text('Donation History'),
-            ),
-            SizedBox(height: 8.0),
-            ElevatedButton(
-              onPressed: () {
-                // handle e-certificate button press
-              },
-              child: Text('E-Certificate'),
+        appBar: AppBar(
+          title: const Text('My Profile'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () {},
             ),
           ],
         ),
-      ),
-    );
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Account Details',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Name'),
+              subtitle: Text(widget.user.username),
+            ),
+            ListTile(
+              leading: const Icon(Icons.email),
+              title: const Text('Email'),
+              subtitle: Text(widget.user.email),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Dark Mode',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  Spacer(),
+                  Switch(
+                    value: _isDarkModeEnabled,
+                    onChanged: (value) {
+                      setState(() {
+                        _isDarkModeEnabled = value;
+                      });
+                      // TODO: Implement dark mode functionality
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'History',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 500,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(width: 8),
+                  ElevatedButton(
+                    onPressed: () {
+                      // navigate to your request page
+                    },
+                    child: const Text('Your Request'),
+                  ),
+                  const SizedBox(width: 8),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Donation History'),
+                  ),
+                  const SizedBox(width: 8),
+                  const SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PdfList(
+                              userId: widget.user.userid
+                                  , // pass userId as a string
+                            ),
+                          ));
+                    },
+                    child: const Text('E-Certificate'),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
