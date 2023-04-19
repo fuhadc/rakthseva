1ab43193832484e54e565de006cdef174dedcaf5
fuhadc <fuhadcs@gmail.com>
2023-04-19 13:01:01 +0530

    dsa

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -33,11 +33,10 @@ class _HomePageState extends State<HomePage> {
   Future<List<String>> _fetchImages() async {
     await Future.delayed(const Duration(seconds: 2)); // Simulate network delay
     return [
-      'https://via.placeholder.com/500x250',
-      'https://via.placeholder.com/500x251',
-      'https://via.placeholder.com/500x252',
-      'https://via.placeholder.com/500x253',
-      'https://via.placeholder.com/500x254',
+      'https://user-images.githubusercontent.com/97867059/233001055-40730431-37b3-49b0-9cd6-a8d78eb602d0.jpeg',
+      'https://user-images.githubusercontent.com/97867059/233001083-76e1d768-0791-47f4-ad7e-83889b4f0029.jpeg',
+      'https://user-images.githubusercontent.com/97867059/233001086-4d6add70-38d9-43f6-9267-6e30ffcce0f2.jpeg',
+      'https://user-images.githubusercontent.com/97867059/233001092-e59af73a-22ff-405e-8947-2063004653ba.jpeg',
     ];
   }
 
